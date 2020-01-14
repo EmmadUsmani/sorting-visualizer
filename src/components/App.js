ceaa117c99b3aac34999ab80810541cfee13e9e2
Emmad Usmani <emmadausmani@gmail.com>
2020-01-14 10:33:49 -0800

    implemented dropdown and sliders; UI/options state in app

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,31 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import Slider from '@material-ui/core/Slider';
 import './App.css'
 
 class App extends React.Component {
+  state = {
+    algorithms: ["Merge Sort", "Quick Sort", "Heap Sort", "Insertion Sort", "Bubble Sort"],
+    selectedAlgo: "Merge Sort",
+    speed: 100,
+    items: 100
+  }
+
+  setAlgo = (e) => {
+    this.setState({selectedAlgo: e.target.value});
+  }
+
+  setSpeed = (e, value) => {
+    this.setState({speed: value});
+  }
+
+  setItems = (e, value) => {
+    this.setState({items: value});
+  }
+
   render() {
     return (
         <>
@@ -21,19 +38,20 @@ class App extends React.Component {
           <CardContent>
             <Typography>Algorithm</Typography>
             <FormControl className="dropdown">
-              <Select value="Merge Sort">
-                  <MenuItem value="Merge Sort">Merge Sort</MenuItem>
-                  <MenuItem value="Quick Sort">Quick Sort</MenuItem>
+              <Select value={this.state.selectedAlgo} onChange={this.setAlgo}>
+                  {this.state.algorithms.map((a) => <MenuItem value={a}>{a}</MenuItem>)}
               </Select>
             </FormControl>
             <Typography>Speed</Typography>
-            <Slider value={0} type="speed" valueLabelDisplay="auto"/>
-            <Typography>Elements</Typography>
-            <Slider value={0} type="elements" valueLabelDisplay="auto"/>
+            <Slider value={this.state.speed} onChange={this.setSpeed} min={1} valueLabelDisplay="auto"/>
+            <Typography>Items</Typography>
+            <Slider value={this.state.items} onChange={this.setItems} min={5} valueLabelDisplay="auto"/>
           </CardContent>
-          <CardActions className="buttons">
-            <Button color="primary" text="sort">Sort</Button>
-            <Button color="secondary" text="shuffle">Shuffle</Button>
+          <CardActions className="actions">
+            <div className="buttons">
+              <Button color="primary" text="sort">Sort</Button>
+              <Button color="primary" text="shuffle">Shuffle</Button>
+            </div>
           </CardActions>
         </Card>
         <Canvas/>
