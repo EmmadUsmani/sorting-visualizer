37845e59a972843d24d430a74a7bb1c63afc09fe
Emmad Usmani <emmadausmani@gmail.com>
2020-01-14 08:09:45 -0800

    setup basic canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,12 +2,31 @@ import React from 'react';
 import './Canvas.css'
 
 class Canvas extends React.Component {
-    canvas = React.createRef();
+    canvasRef = React.createRef();
+
+    componentDidMount() {
+        window.addEventListener("resize", this.resizeCanvas)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resizeCanvas);
+    }
+
+    resizeCanvas = () => {
+        this.canvasRef.current.width = window.innerWidth;
+        this.canvasRef.current.height = window.innerHeight;
+        this.draw();
+    }
+
+    draw() {
+        const ctx = this.canvasRef.current.getContext('2d');
+        ctx.fillRect(100, 100, 100, 100);
+    }
 
     render() {
         return (
             <canvas 
-            ref={this.canvas} 
+            ref={this.canvasRef} 
             width={window.innerWidth} 
             height={window.innerHeight}/>
         );
