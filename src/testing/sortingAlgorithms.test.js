519a1e1ec589c872256830b355b04e4d27703f17
Emmad Usmani <emmadausmani@gmail.com>
2020-01-15 16:08:55 -0800

    added test for insertion sort

--- src/testing/sortingAlgorithms.test.js
+++ src/testing/sortingAlgorithms.test.js
@@ -3,3 +3,7 @@ import testSort from './testSort';
 describe('mergeSort', () => {
     it('should correctly sort ItemLists of size 1 to 100', testSort('Merge Sort'));
 });
+
+describe('insertionSort', () => {
+    it('should correctly sort ItemLists of size 1 to 100', testSort('Insertion Sort'));
+});
